83f58de857bbbcd4e882f179472fa17fef3303ad
Yokomi <38473135+Yokomide@users.noreply.github.com>
2022-03-20 17:12:39 +0300

    Add Project

---
+++ CURSED/Assets/Scripts/FPS_Lock.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_Lock : MonoBehaviour
+{
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+}
