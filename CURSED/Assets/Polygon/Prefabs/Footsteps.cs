c8821e94439d44db985b516aec5dc1c1ea191fcc
Yokomi <38473135+Yokomide@users.noreply.github.com>
2022-04-01 01:13:12 +0300

    Update
    add: footstep and nature asset
    add: footsteps script

---
+++ CURSED/Assets/Polygon/Prefabs/Footsteps.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Step()
+    {
+        AudioClip clip = GetRandomStepClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    public void Fall()
+    {
+        AudioClip clip = clips[11];
+        audioSource.PlayOneShot(clip);
+    }
+
+
+    private AudioClip GetRandomStepClip()
+    {
+        return clips[Random.Range(0, 10)];
+    }
+}
